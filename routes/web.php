75262e3906cddf05085f41481d2ec84963e0329f
Manuel. R. Mezquita. <mrodriguezmezquita@gmail.com>
2024-07-29 12:07:35 -0600

    Una diminuta prueba para la integración de AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//rutas para administrador
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index')->middleware('auth');
